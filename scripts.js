9d81788dba5428ff744828981b284fe7cd76a2f0
Damian Giedrojć <icelookpl@gmail.com>
2018-05-13 21:50:22 +0200

    circle class and randomizing

--- scripts.js
+++ scripts.js
@@ -1,78 +1,88 @@
 'use strict';
 
 const canvas = document.querySelector('#guesser');
-const ctx = canvas.getContext('2d');  
-const divider = 0.7;   
+const ctx = canvas.getContext('2d');   
+
+const randomix = (z) => {
+    let result = Math.floor((Math.random() * z) + 1 );
+    return result;
+}
+
+const randomixRGB = () => {
+    let result = `rgb(${randomix(255)}, ${randomix(255)}, ${randomix(255)})`;
+    return result;
+}
+
+let sprites = {
+    circles : [],
+};
+let spritesCounter = 0;
+
+class Circle {
+    constructor(x, y, radius, strokeWidth, color1, color2, rotateSpeed) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius,
+        this.strokeWidth = strokeWidth;
+        this.color1 = color1;
+        this.color2 = color2;
+        this.gradientPos = [this.x - (this.strokeWidth/2 + this.radius/2), this.y - (this.strokeWidth/2 + this.radius/2), this.x + (this.strokeWidth/2 + this.radius/2), this.y + (this.strokeWidth/2 + this.radius/2)]
+        this.gradientColor = ctx.createLinearGradient(this.gradientPos[0], this.gradientPos[1], this.gradientPos[2], this.gradientPos[3]);
+        this.rotateSpeed = rotateSpeed;
+        this.rotateAngle = 0;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.lineWidth = this.strokeWidth;
+        this.gradientColor.addColorStop('0', this.color1);
+        this.gradientColor.addColorStop('1', this.color2);
+        ctx.strokeStyle = this.gradientColor;
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        ctx.stroke();
+    }
+
+    move() {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate((Math.PI / 180) * this.rotateAngle);
+        ctx.beginPath();
+        
+        ctx.lineWidth = this.strokeWidth;
+        this.gradientPos = [-(this.strokeWidth/2 + this.radius/2), -(this.strokeWidth/2 + this.radius/2), this.strokeWidth/2 + this.radius/2, this.strokeWidth/2 + this.radius/2];
+        this.gradientColor = ctx.createLinearGradient(this.gradientPos[0], this.gradientPos[1], this.gradientPos[2], this.gradientPos[3]);
+        this.gradientColor.addColorStop('0', this.color1);
+        this.gradientColor.addColorStop('1', this.color2);
+
+        ctx.strokeStyle  = this.gradientColor;
+        ctx.arc(0, 0, this.radius, 0, 2 * Math.PI);
+        ctx.fillStyle = '#222222';
+        ctx.fill();
+        ctx.stroke();
+
+        this.rotateAngle += this.rotateSpeed;
+        ctx.restore();
+    }
+}
+
+
+function draw() {
+    sprites.circles[spritesCounter] = new Circle(randomix(600), randomix(600), 10, 7, randomixRGB(), randomixRGB(), 25);
+
+    ctx.clearRect(0,0, 600, 600); 
+    
+    for(let i = 0; i < spritesCounter; i++) {
+        sprites.circles[i].move();
+    }
+    
+    spritesCounter++;
+    console.log(spritesCounter)
+    window.requestAnimationFrame(draw);
+
+}
 
 let init = () => {
     window.requestAnimationFrame(draw);
 }
 
-let randomix = (z) => {
-    let result = Math.floor((Math.random() * z) + 1 );
-    return result;
-}
-
-let i = 1;
-let randomGradientColor;
-let r = randomix(255);
-let g = randomix(255);
-let b = randomix(255);
-let r1 = randomix(255);
-let g1 = randomix(255);
-let b1 = randomix(255);
-let circleX;
-let circleY;
-let circleLineWidth;
-let circleRadius;
-let gradientPos = [];
-
-function draw() {
-
-    ctx.clearRect(0,0, 600, 600); 
-    
-    circleX = 300;
-    circleY = 300;
-    circleLineWidth = 10;
-    circleRadius = 30;
-
-    gradientPos = [circleX - (circleLineWidth/2 + circleRadius/2), circleY - (circleLineWidth/2 + circleRadius/2), circleX + (circleLineWidth/2 + circleRadius/2), circleY + (circleLineWidth/2 + circleRadius/2)];
-
-    ctx.beginPath();
-    ctx.lineWidth = circleLineWidth;
-    randomGradientColor = ctx.createLinearGradient(gradientPos[0], gradientPos[1], gradientPos[2], gradientPos[3]);    
-    randomGradientColor.addColorStop("0",`rgb(${r},${g},${b}`);
-    randomGradientColor.addColorStop("1.0",`rgb(${r1},${g1},${b1}`);
-    ctx.strokeStyle  = randomGradientColor;
-    ctx.arc(circleX, circleY, circleRadius, 0, 2 * Math.PI);
-    ctx.stroke();
-
-    ctx.save();
-
-    ctx.translate(circleX, circleY);
-
-
-
-    ctx.rotate((Math.PI / 180) * i)
-    ctx.beginPath();
-    randomGradientColor = ctx.createLinearGradient(-20, -20, 20, 20);
-    randomGradientColor.addColorStop("0",`rgb(${r},${g},${b}`);
-    randomGradientColor.addColorStop("1.0",`rgb(${r1},${g1},${b1}`);
-    ctx.strokeStyle  = randomGradientColor;
-    ctx.arc(0, 0, circleRadius, 0, 2 * Math.PI);
-    ctx.stroke();
-    /*
-    ctx.arc(70, 70, circleRadius, 0, 2 * Math.PI);
-    ctx.strokeStyle  = randomGradientColor;
-    ctx.stroke();
-
-*/
-    ctx.restore();
-    i += 10;
-    console.log(i)
-
-
-    window.requestAnimationFrame(draw);
-
-}
 init();
